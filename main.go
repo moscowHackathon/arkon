3c952c23763667aa6fbca0ad1312ff7dc5543037
Andrey Perepechaev <andrey.perepechaev@lazada.com>
2016-07-02 11:19:48 +0300

    go fmt

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	_ "github.com/moskowhackaton/arkon/routers"
 	"github.com/astaxie/beego"
+	_ "github.com/moskowhackaton/arkon/routers"
 )
 
 func main() {
 	beego.Run()
 }
-
--- routers/router.go
+++ routers/router.go
@@ -1,10 +1,10 @@
 package routers
 
 import (
-	"github.com/moskowhackaton/arkon/controllers"
 	"github.com/astaxie/beego"
+	"github.com/moskowhackaton/arkon/controllers"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
 }
